6f037448d080227c56cc4288a6082a52f31178a1
Dominik <dominik.szlaza@gazeta.pl>
2023-07-07 22:23:23 +0200

    refactor: remove default endpoints

--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from fastapi import FastAPI
 
 from app.core.config import settings
@@ -11,13 +9,3 @@ app = FastAPI(
 )
 
 app.include_router(api_router)
-
-
-@app.get("/")
-async def root() -> Any:
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str) -> Any:
-    return {"message": f"Hello {name}"}
