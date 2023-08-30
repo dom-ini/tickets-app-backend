4d6d8004205abcb83cb3e03e1eafc9804670e89c
Dominik <dominik.szlaza@gazeta.pl>
2023-08-30 16:04:05 +0200

    test: change class-based config tests to function-based

--- app/tests/unit/core/test_config.py
+++ app/tests/unit/core/test_config.py
@@ -4,20 +4,21 @@ from pydantic import ValidationError
 from app.core.config import Settings
 
 
-class TestConfig:
-    def test_default_from_name(self) -> None:
-        default_from = "DEFAULT_FROM"
-        project_name = "PROJECT_NAME"
-        config = Settings(DEFAULT_FROM_NAME=default_from, PROJECT_NAME=project_name)
-        assert config.DEFAULT_FROM_NAME == default_from
+def test_default_from_name() -> None:
+    default_from = "DEFAULT_FROM"
+    project_name = "PROJECT_NAME"
+    config = Settings(DEFAULT_FROM_NAME=default_from, PROJECT_NAME=project_name)
+    assert config.DEFAULT_FROM_NAME == default_from
 
-    def test_default_from_name_should_default_to_project_name(self) -> None:
-        project_name = "PROJECT_NAME"
-        config = Settings(PROJECT_NAME=project_name)
-        assert config.DEFAULT_FROM_NAME == project_name
 
-    def test_emails_enabled_with_no_configuration_should_fail(self) -> None:
-        emails_enabled = True
-        smtp_host = ""
-        with pytest.raises(ValidationError):
-            Settings(EMAILS_ENABLED=emails_enabled, SMTP_HOST=smtp_host)
+def test_default_from_name_should_default_to_project_name() -> None:
+    project_name = "PROJECT_NAME"
+    config = Settings(PROJECT_NAME=project_name)
+    assert config.DEFAULT_FROM_NAME == project_name
+
+
+def test_emails_enabled_with_no_configuration_should_fail() -> None:
+    emails_enabled = True
+    smtp_host = ""
+    with pytest.raises(ValidationError):
+        Settings(EMAILS_ENABLED=emails_enabled, SMTP_HOST=smtp_host)
