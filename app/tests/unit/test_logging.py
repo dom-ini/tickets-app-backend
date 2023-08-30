2edad7d24bd0ff2054f85d07f2d11d240da6eebf
Dominik <dominik.szlaza@gazeta.pl>
2023-08-30 16:05:32 +0200

    test: change class-based logging tests to function-based

--- app/tests/unit/test_logging.py
+++ app/tests/unit/test_logging.py
@@ -3,29 +3,32 @@ import logging
 from app.logging import create_stdout_handler, set_up_logger
 
 
-class TestLogging:
-    def test_create_stdout_handler_creates_stream_handler(self) -> None:
-        handler = create_stdout_handler()
-        assert isinstance(handler, logging.StreamHandler)
+def test_create_stdout_handler_creates_stream_handler() -> None:
+    handler = create_stdout_handler()
+    assert isinstance(handler, logging.StreamHandler)
 
-    def test_create_stdout_handler_creates_valid_formatter(self) -> None:
-        handler = create_stdout_handler()
-        formatter = handler.formatter
-        assert isinstance(formatter, logging.Formatter)
-        fields = ["asctime", "levelname", "message"]
-        assert all(field in str(formatter._fmt) for field in fields)  # pylint: disable=W0212
 
-    def test_set_up_logger_should_set_name(self) -> None:
-        logger_name = "test_logger"
-        logger = set_up_logger(logger_name)
-        assert logger.name == logger_name
+def test_create_stdout_handler_creates_valid_formatter() -> None:
+    handler = create_stdout_handler()
+    formatter = handler.formatter
+    assert isinstance(formatter, logging.Formatter)
+    fields = ["asctime", "levelname", "message"]
+    assert all(field in str(formatter._fmt) for field in fields)  # pylint: disable=W0212
 
-    def test_set_up_logger_should_set_info_level(self) -> None:
-        logger_name = "test_logger"
-        logger = set_up_logger(logger_name)
-        assert logger.level == logging.INFO
 
-    def test_set_up_logger_should_set_stream_handler(self) -> None:
-        logger_name = "test_logger"
-        logger = set_up_logger(logger_name)
-        assert any(isinstance(handler, logging.StreamHandler) for handler in logger.handlers)
+def test_set_up_logger_should_set_name() -> None:
+    logger_name = "test_logger"
+    logger = set_up_logger(logger_name)
+    assert logger.name == logger_name
+
+
+def test_set_up_logger_should_set_info_level() -> None:
+    logger_name = "test_logger"
+    logger = set_up_logger(logger_name)
+    assert logger.level == logging.INFO
+
+
+def test_set_up_logger_should_set_stream_handler() -> None:
+    logger_name = "test_logger"
+    logger = set_up_logger(logger_name)
+    assert any(isinstance(handler, logging.StreamHandler) for handler in logger.handlers)
