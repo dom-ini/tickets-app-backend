f2a4ead31b55b3d086cada48843d9c58f7f343f1
Dominik <dominik.szlaza@gazeta.pl>
2023-08-31 16:15:54 +0200

    refactor: rename crud-related modules

--- app/auth/crud/__init__.py
+++ app/auth/crud/__init__.py
@@ -1,5 +1,5 @@
-from app.auth.crud.password_reset import CRUDPasswordResetToken
-from app.auth.crud.user import CRUDUser
+from app.auth.crud.crud_password_reset import CRUDPasswordResetToken
+from app.auth.crud.crud_user import CRUDUser
 from app.auth.models import PasswordResetToken, User
 
 user = CRUDUser(User)
--- app/events/crud/__init__.py
+++ app/events/crud/__init__.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
 
 from app.common.crud import CRUDBase
-from app.events.crud.artist import CRUDArtist
-from app.events.crud.event import CRUDEvent
-from app.events.crud.event_type import CRUDEventType
+from app.events.crud.crud_artist import CRUDArtist
+from app.events.crud.crud_event import CRUDEvent
+from app.events.crud.crud_event_type import CRUDEventType
 from app.events.models import Artist, Event, EventType, Location, Organizer
 from app.events.schemas import LocationCreate, OrganizerCreate
 
