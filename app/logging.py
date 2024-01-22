452a94e31db4a230006bddf9c305c75d677a6b42
Dominik <dominik.szlaza@gazeta.pl>
2024-01-22 20:50:57 +0100

    fix: fix circular import in logging package

--- app/logging.py
+++ app/logging.py
@@ -1,17 +1,17 @@
-import logging
+import logging as log
 import sys
 
 
-def create_stdout_handler() -> logging.Handler:
-    handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+def create_stdout_handler() -> log.Handler:
+    handler = log.StreamHandler(sys.stdout)
+    formatter = log.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     return handler
 
 
-def set_up_logger(name: str) -> logging.Logger:
-    new_logger = logging.getLogger(name)
-    new_logger.setLevel(logging.INFO)
+def set_up_logger(name: str) -> log.Logger:
+    new_logger = log.getLogger(name)
+    new_logger.setLevel(log.INFO)
     handler = create_stdout_handler()
     new_logger.addHandler(handler)
     return new_logger
