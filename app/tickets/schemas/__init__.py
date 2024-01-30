ecb2595e6e3ed5b76a88ee1a3687c5a0ed7c40e6
Dominik <dominik.szlaza@gazeta.pl>
2024-01-30 11:55:51 +0100

    fix: remove email address from get ticket by token response

--- app/tickets/schemas/__init__.py
+++ app/tickets/schemas/__init__.py
@@ -1,10 +1,11 @@
-from .ticket import Ticket, TicketCreate, TicketCreateBody, TicketWithEvent, TicketWithUser
+from .ticket import SafeTicketWithEvent, Ticket, TicketCreate, TicketCreateBody, TicketWithEvent, TicketWithUser
 from .ticket_category import TicketCategory, TicketCategoryCreate, TicketCategoryWithEvent, TicketCategoryWithLeftCount
 
 __all__ = [
     "Ticket",
     "TicketWithUser",
     "TicketWithEvent",
+    "SafeTicketWithEvent",
     "TicketCreate",
     "TicketCreateBody",
     "TicketCategory",
