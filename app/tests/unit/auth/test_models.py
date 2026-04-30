88e27bca6e666c2bf05fcf7345647fb303e64312
Dominik <mrdominino@gmail.com>
2026-04-30 21:07:32 +0200

    test: improve typing in auth models tests

--- app/tests/unit/auth/test_models.py
+++ app/tests/unit/auth/test_models.py
@@ -1,11 +1,20 @@
 from datetime import datetime
-from typing import Any, Type
+from typing import Any, ParamSpec, Protocol, Type, TypeVar
 
 import pytest
 
 from app.auth.models import PasswordResetToken, User, VerificationToken
-from app.db.base_class import Base
 from app.tests.unit.utils import create_user_instance
+
+P = ParamSpec("P")
+R_co = TypeVar("R_co", covariant=True)
+
+
+class ModelWithUser(Protocol[P, R_co]):
+    user: User
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R_co:
+        ...
 
 
 def test_str_user() -> None:
@@ -17,7 +26,7 @@ def test_str_user() -> None:
 @pytest.mark.parametrize(
     "model,additional_payload", [(PasswordResetToken, {"expires_at": datetime.utcnow()}), (VerificationToken, {})]
 )
-def test_str_tokens(model: Type[Base], additional_payload: dict[str, Any], user_instance: User) -> None:
+def test_str_tokens(model: Type[ModelWithUser], additional_payload: dict[str, Any], user_instance: User) -> None:
     payload = {"value": "token-value", "user_id": 1, **additional_payload}
     token = model(**payload)
     token.user = user_instance
